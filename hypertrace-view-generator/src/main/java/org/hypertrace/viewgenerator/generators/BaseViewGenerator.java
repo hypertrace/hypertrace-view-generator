fc501533fc2b853ebfcef74b5e234207288bb388
samarth-gupta-traceable <samarth@traceable.ai>
2020-08-19 00:16:37 +0530

    view gen arrival lag metric

--- hypertrace-view-generator/src/main/java/org/hypertrace/viewgenerator/generators/BaseViewGenerator.java
+++ hypertrace-view-generator/src/main/java/org/hypertrace/viewgenerator/generators/BaseViewGenerator.java
@@ -7,6 +7,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import javax.annotation.Nullable;
+
+import io.micrometer.core.instrument.Timer;
 import org.apache.avro.generic.GenericRecord;
 import org.hypertrace.core.datamodel.AttributeValue;
 import org.hypertrace.core.datamodel.Attributes;
@@ -16,15 +18,25 @@ import org.hypertrace.core.datamodel.EventRef;
 import org.hypertrace.core.datamodel.EventRefType;
 import org.hypertrace.core.datamodel.MetricValue;
 import org.hypertrace.core.datamodel.StructuredTrace;
+import org.hypertrace.core.datamodel.shared.DataflowMetric;
+import org.hypertrace.core.datamodel.shared.DataflowMetricUtils;
+import org.hypertrace.core.serviceframework.metrics.PlatformMetricsRegistry;
 import org.hypertrace.core.viewgenerator.JavaCodeBasedViewGenerator;
 import org.hypertrace.traceenricher.enrichedspan.constants.EnrichedSpanConstants;
 import org.hypertrace.traceenricher.enrichedspan.constants.v1.CommonAttribute;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Pre-processing for View Generators, for data that are mostly needed by the the view generators
  */
 public abstract class BaseViewGenerator<OUT extends GenericRecord>
     implements JavaCodeBasedViewGenerator<StructuredTrace, OUT> {
+
+  private static Logger LOGGER = LoggerFactory.getLogger(BaseViewGenerator.class);
+  private static final String VIEW_GENERATION_ARRIVAL_LAG = "view.generation.arrival.lag";
+  private static final Timer viewGeneratorArrivalTimer = PlatformMetricsRegistry
+      .registerTimer(VIEW_GENERATION_ARRIVAL_LAG, new HashMap<>());
 
   static final String EMPTY_STRING = "";
 
@@ -56,6 +68,8 @@ public abstract class BaseViewGenerator<OUT extends GenericRecord>
 
   @Override
   public List<OUT> process(StructuredTrace trace) {
+    DataflowMetricUtils.reportArrivalLagAndInsertTimestamp(trace, viewGeneratorArrivalTimer,
+        DataflowMetric.VIEW_GENERATION_ARRIVAL_TIME);
     Map<String, Entity> entityMap = new HashMap<>();
     Map<ByteBuffer, Event> eventMap = new HashMap<>();
     Map<ByteBuffer, List<ByteBuffer>> parentToChildrenEventIds = new HashMap<>();
