a85ffde347ea44427f513f369f27b68a508901b5
GurtejSohi <gurtejsohi25@gmail.com>
2020-10-27 22:55:41 +0530

    Handle cases where HTTP URL tags do not contain full URL

--- hypertrace-view-generator/build.gradle.kts
+++ hypertrace-view-generator/build.gradle.kts
@@ -31,7 +31,7 @@ dependencies {
   implementation("org.hypertrace.core.datamodel:data-model:0.1.9")
   implementation("org.hypertrace.core.serviceframework:platform-metrics:0.1.15")
 
-  implementation("org.hypertrace.traceenricher:enriched-span-constants:0.1.23")
+  implementation("org.hypertrace.traceenricher:enriched-span-constants:0.1.23") // TODO: upgrade its version once the related PR is merged in trace-enricher
   implementation("org.hypertrace.traceenricher:hypertrace-trace-enricher-api:0.1.23")
   implementation("org.hypertrace.core.spannormalizer:raw-span-constants:0.1.22")
   implementation("org.hypertrace.entity.service:entity-service-api:0.1.21")
