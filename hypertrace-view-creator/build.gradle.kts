97b5c8f9e76e1e7ad0ca9e7505dd76cb5ac07d69
Buchi Reddy Busi Reddy <buchi@traceable.ai>
2020-11-06 16:31:43 -0800

    Upgrading view-creator library.

--- hypertrace-view-creator/build.gradle.kts
+++ hypertrace-view-creator/build.gradle.kts
@@ -17,7 +17,12 @@ tasks.test {
 
 dependencies {
   implementation(project(":hypertrace-view-generator-api"))
-  implementation("org.hypertrace.core.viewcreator:view-creator-framework:0.1.16")
+  implementation("org.hypertrace.core.viewcreator:view-creator-framework:0.1.17")
+  constraints {
+    implementation("com.google.guava:guava:30.0-android") {
+      because("Information Disclosure (new) [Medium Severity][https://snyk.io/vuln/SNYK-JAVA-COMGOOGLEGUAVA-1015415] in com.google.guava:guava@29.0-android")
+    }
+  }
 
   testImplementation("org.junit.jupiter:junit-jupiter:5.6.2")
   testImplementation("org.mockito:mockito-core:3.3.3")
